87e4c43d76a4f153045e0ca389afc72ef4169909
jagosyafaat30 <syafaatmm30@gmail.com>
2025-12-10 23:20:43 +0700

    Refactor CheckAnswerSchema validation for question and answer

--- src/api/game/game-list/find-the-match/schema/check-answer.schema.ts
+++ src/api/game/game-list/find-the-match/schema/check-answer.schema.ts
@@ -1,12 +1,8 @@
 import { z } from 'zod';
 
 export const CheckAnswerSchema = z.object({
-  question: z.string({
-    required_error: 'Question is required',
-  }),
-  answer: z.string({
-    required_error: 'Answer is required',
-  }),
+  question: z.string().min(1, 'Question is required'),
+  answer: z.string().min(1, 'Answer is required'),
   remaining_answers: z.array(z.string()).optional(),
   current_lives: z
     .number()
